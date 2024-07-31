649b3137e378731624ddfb064a0c73a6c2add717
wbguanna <jiwhan13@kakao.com>
2024-07-31 07:11:17 +0000

    일단 강사님 코드대로 저장

--- server/app.js
+++ server/app.js
@@ -4,89 +4,74 @@ const app = express()
 
 const port = 8000;
 
+// 뷰 템플릿 상용을 위한 설정
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
-// https://expressjs.com/en/starter/static-files.html
 // /public/index.html을 보여주기 위한 static 폴더 지정.
 app.use(express.static('public'));
+
+// 사람 데이터 목록 선언
+const saramList = [
+    {no:102, name:'홍길동', email:'hong@saram.com', job:'도둑', age:23},
+    {no:101, name:'이길동', email:'lee@saram.com', job:'변호사', age:33},
+    {no:103, name:'김길순', email:'kim@saram.com', job:'프로그래머', age:27},
+    {no:104, name:'박길순', email:'park@saram.com', job:'군인', age:25}
+];
 
 // localhost:8000/saram
 app.get('/saram', function(req, res) {
     // ejs 페이지로 렌더링 
     // - views/saram.ejs 페이지의 코드를 읽어와서 res.end()에 적용한다.
 
-    var message_data = "Hello world ㅋㅋㅋ";
-    req.app.render('saram', {message_data}, function(err, html) {
+    var message = "사람 정보 관리 페이지";
+    req.app.render('saram', {message, saramList}, function(err, html) {
         res.end(html);
     });
 });
 
-// app.get('/saram/update', function(req, res) {
-//   console.log("GET - /saram/update >>> ", req.query);
-//   // saramList에서 해당 정보를 찾아서 update 하기.
-//   res.send(req.query);
-// });
-
-app.get('/saram/update', function(req, res) { // 김동영씨 코드..
-  console.log("GET - /saram/update >>> ", req.query);
-  // saramList에서 해당 정보를 찾아서 update 하기.
-  var idx = saramList.findIndex(function(saram) {
-      return saram.no == req.query.no;
-  });
-  var saram = null;
-  if (idx!= -1) {
-      saram = saramList[idx];
-  }
-  saram.name = req.query.name;
-  saram.email = req.query.email;
-  saram.job = req.query.job;
-  saram.age = parseInt(req.query.age);
-  
-  res.send(req.query);
+// localhost:8000/saram/detail?no=103
+app.get('/saram/detail', function(req, res) {
+    console.log("GET - /saram/detail >>>> no: " + req.query.no);
+    var idx = saramList.findIndex(function(saram) {
+        return saram.no == req.query.no;
+    });
+    var saram = null;
+    if(idx != -1) {
+        saram = saramList[idx];
+    }
+    req.app.render('saramDetail', {saram}, function(err, html) {
+        res.end(html);
+    });
 });
 
-// localhost:8000/saram/update?no=103
+app.get('/saram/edit', function(req, res) {
+    console.log("GET - /saram/edit >>>> no: " + req.query.no);
+    var idx = saramList.findIndex(function(saram) {
+        return saram.no == req.query.no;
+    });
+    var saram = null;
+    if(idx != -1) {
+        saram = saramList[idx];
+    }
+    req.app.render('saramEdit', {saram}, function(err, html) {
+        res.end(html);
+    });
+});
+
 app.get('/saram/update', function(req, res) {
-  console.log('GET - /saram/update >>>> no: ' + req.query.no);
-  var idx = saramList.findIndex(function(saram) {
+    console.log("GET - /saram/update >>> ", req.query);
+    // saramList에서 해당 정보를 찾아서 update 하기.
+    saramList.findIndex(function(saram) {
       return saram.no == req.query.no;
-  });
-  var saram = req.query;
-  if(idx != -1) {
-      saramList[idx] = saram;
-  }
-  res.redirect('/saram');
-})
+    })
 
-
-// app.get('/saram/update', (req, res) => { // 준혁쿤 코드..
-//   const updatedSaram = {
-//       no: parseInt(req.query.no, 10),
-//       name: req.query.name,
-//       email: req.query.email,
-//       job: req.query.job,
-//       age: req.query.age,
-//   };
-
-//   const idx = saramList.findIndex((saram) => saram.no === updatedSaram.no);
-//   if (idx === -1) {
-//       res.status(404).send('사람을 찾을 수 없습니다.');
-//       return;
-//   }
-
-//   saramList[idx] = updatedSaram;
-
-//   // 업데이트 후 /saram/detail로 리디렉션
-//   res.redirect(`/saram/detail?no=${updatedSaram.no}`);
-// });
-
-
+    res.send(req.query);
+});
 
 const server = http.createServer(app);
 server.listen(port, function() {
     console.log("서버 실행 중 >>> http://localhost:"+port);
 });
-
 
 // 실제 웹 서버 구축에서는 Nodejs만 사용하지 않고 express를 더 많이 사용합니다.
