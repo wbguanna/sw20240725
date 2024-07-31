85949a207f9e14f3e0c94fd2d1f9ded33fd4cb67
wbguanna <jiwhan13@kakao.com>
2024-07-31 05:47:15 +0000

    day05 강사님 코드 따라가기

--- server/app.js
+++ server/app.js
@@ -1,10 +1,30 @@
-const http = require("http");
+const http = require('http');
+const express = require('express');
+const app = express()
 
-const server = http.createServer(function (req, res) {
-  res.write("<html><body>Hello World!</body></html>"); //write a response to the client
-  res.end(); //end the response
+const port = 8000;
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+// https://expressjs.com/en/starter/static-files.html
+// /public/index.html을 보여주기 위한 static 폴더 지정.
+app.use(express.static('public'));
+
+// localhost:8000/saram
+app.get('/saram', function(req, res) {
+    // ejs 페이지로 렌더링 
+    // - views/saram.ejs 페이지의 코드를 읽어와서 res.end()에 적용한다.
+
+    var message_data = "Hello world ㅋㅋㅋ";
+    req.app.render('saram', {message_data}, function(err, html) {
+        res.end(html);
+    });
 });
 
-server.listen(3000, function () {
-  console.log("서버 실행 중 >>> http://localhost:3000");
+const server = http.createServer(app);
+server.listen(port, function() {
+    console.log("서버 실행 중 >>> http://localhost:"+port);
 });
+
+// 실제 웹 서버 구축에서는 Nodejs만 사용하지 않고 express를 더 많이 사용합니다.
